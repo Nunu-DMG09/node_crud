947aff4e8bb3bf2918cc8fe7a7e1be3e70a8309e
Nunu-DMG09 <davidmesta09@gmail.com>
2025-10-16 11:45:23 -0500

    refactor router.js to use res.render for index, create, and edit routes

--- router.js
+++ router.js
@@ -7,8 +7,19 @@ const conexion = require('./database/db');
     PETICIONES :request/resq/salida
 */ 
 
-router.get('/', (req, res) => {
-    res.send('HOLA NODE JS');
-})
+router.get('', (req, res) => {
+    //res.send('SENATI: INSTITUTO TECNOLOGICO SUPERIOR');
+    res.render("index");
+});
+
+router.get('/create', (req, res) => {
+    //res.send('DESARROLLADOR: DAVID MESTA GONZALES =) ༼ つ ◕_◕ ༽つ');
+    res.render("create");
+});
+
+router.get('/edit', (req, res) => {
+    res.render('edit');
+});
+
 
 module.exports = router;
